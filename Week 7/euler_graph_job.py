603969311087ae0455d2a68aa4d9304e4a2c20af
Polochon-street <polochonstreet@gmx.fr>
2017-10-29 15:26:10 +0100

    Added output for ex2

--- Week 7/euler_graph_job.py
+++ Week 7/euler_graph_job.py
@@ -2,14 +2,17 @@ from mrjob.job import MRJob
 from collections import Counter
 
 
+# Separate file for the MrEulerGraph class
 class MREulerGraph(MRJob):
+    # Map step
     def mapper(self, key, line):
+        # Each line is "node1, node2" so make it a list like [node1, node2]
         nodes = list(map(int, line.split()))
+        # Add 1 to each edge that appears
         yield nodes[0], 1
         yield nodes[1], 1
 
     def reducer(self, key, values):
+        # For each node, return True if the sum of its edges is even,
+        # and False if it's odd
         yield key, (sum(values) % 2) == 0
-
-if __name__ == '__main__':
-    MREulerGraph.run()
--- Week 7/ex7.2.py
+++ Week 7/ex7.2.py
@@ -1,6 +1,6 @@
 from euler_graph_job import MREulerGraph
-from sys import stdin
 
+# List of the test file
 files = [
     'graph1.txt',
     'graph2.txt',
@@ -9,16 +9,24 @@ files = [
     'graph5.txt',
 ]
 
+# Test the code for each file
 for graph_file in files:
+    # Create the job
     mr_job = MREulerGraph([graph_file])
-    
+
+    # Use a with structure to automatically destroy the runner when finished
     with mr_job.make_runner() as runner:
+        # Run the job
         runner.run()
         euler_graph = True
+        # Parse each output line after having ran MapReduce
         for line in runner.stream_output():
-            key, value = mr_job.parse_output_line(line)
+            # Check if one of the value is false; if it is
+            # then it's not an Euler graph
+            _, value = mr_job.parse_output_line(line)
             if euler_graph:
                 euler_graph = value
+        # Print final output 
         if euler_graph:
             print(
                 '{} is the representation of an Euler graph.'
